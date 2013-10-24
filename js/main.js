c29244eec8d6f672974dd24d74ec27d10e4d61d4
shycherry <vinpowful@free.fr>
2013-10-24 18:02:37 +0200

    wip migration to jquery-ui

--- js/main.js
+++ js/main.js
@@ -2,24 +2,26 @@ window.vt = global.vt = require('./v15core/v15tools.js').V15Tools;
 var fs = require('fs');
 var tools = [];
 
+function doLayout(){
+  $('#vt-central').height($(window).height() - 60);
+}
+
 function switchTool(iTool, iCallback){
-  $('.v15main').load(iTool.pathToDir+'/layout.html', function(){
+  $('#vt-main').load(iTool.pathToDir+'/layout.html', function(){
     iCallback(null);
   });
 }
 
-function loadTools(){
+function loadTools(){  
   vt.loadTools(function(err, iTools){
     if(!err){
       tools = iTools;      
       for(var idx in iTools){
         var iTool = iTools[idx];
-        $('.v15ToolsContainer').append('<li class="v15tool" id="'+iTool.vid+'"><a href="#">'+iTool.name+'</a></li>');
+        $('#vt-tabs').append('<span class="v15tool" id="'+iTool.vid+'">'+iTool.name+'</span>');
       }
       
-      $('.v15tool').click(function(){
-        $('.v15tool').removeClass('active');
-        $(this).addClass('active');
+      $('.v15tool').click(function(){        
         var tool = vt.findModelInArray({vid:$(this).attr('id')}, tools);
         switchTool(tool, function(err, data){
           console.log(tool.name+' loaded');
@@ -30,5 +32,11 @@ function loadTools(){
 }
 
 $(document).ready(function() {
+  doLayout();
+  
+  $(window).resize(function(){
+    doLayout();
+  });
+
   loadTools();
 });
