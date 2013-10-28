5d66095aefda596a0e1b19e3af44915fe5390e4c
shycherry <vinpowful@free.fr>
2013-10-28 10:40:17 +0100

    add rw_filter tool

---
+++ tools/RW_Filter/script.js
@@ -0,0 +1,21 @@
+function isODTKo(iLine){
+	return / : KO, rc =.*/.test(iLine);
+}
+
+exports.init = function(){
+  $('#rwf_filter_btn').click(function(){
+  	var lines=[];
+  	var total_output='';
+  	var total_input = $('#rwf_input').val();
+
+  	lines = total_input.split('\n');
+
+  	for(var i in lines){
+  		if(isODTKo(lines[i])){
+  			total_output+=lines[i]+'\n';
+  		}
+  	}
+
+  	$('#rwf_output').val(total_output);
+  });
+}
