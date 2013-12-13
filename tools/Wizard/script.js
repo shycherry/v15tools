58b7a8524e810069a7d56c73573a1540d0c4a765
shycherry <vinpowful@free.fr>
2013-12-13 15:15:29 +0100

    enhance magic

--- tools/Wizard/script.js
+++ tools/Wizard/script.js
@@ -57,6 +57,57 @@ function magicGetModelsFromText(iText){
   return models;
 }
 
+function magicGetModelsFromText2(iText){
+  var regexPathWithExt = /.:[\w\/\\.]+\.[\w]+|[\w\/\\.]+\.[\w]+/gim;
+  var models = [];
+  
+  var matchs;
+  do{    
+    matchs = regexPathWithExt.exec(iText);
+    if(matchs){
+      var currentModels = magicGetModelsFromPath(matchs[0]);
+      for(var idx in currentModels){
+        models.push(currentModels[idx]);
+      }
+    }
+  }while(matchs);
+
+  return models;
+}
+
+function findInTab(iTab, iRegex){
+  if(iRegex){
+    var matchs;
+    for(var idx = 0; idx <= iTab.length; idx++){
+      matchs = iRegex.exec(iTab[idx]);
+      if(matchs){
+        return idx;
+      }
+    }
+  }  
+  return -1;
+}
+
+function findModuleIndex(iTab){
+  var modRegex = /.+\.m$/i;
+  return findInTab(iTab, modRegex);
+}
+
+function findFWInterfacesIndex(iTab){
+  var fwRegex = /(public|protected|private)interfaces/i;
+  return findInTab(iTab, fwRegex);
+}
+
+function findFunctionTestsIndex(iTab){
+  var functionTestsRegex = /functiontests/i;
+  return findInTab(iTab, functionTestsRegex);
+}
+
+function findIdCardIndex(iTab){
+  var idCardRegex = /identitycard/i;
+  return findInTab(iTab, idCardRegex);
+}
+
 function magicGetModelsFromPath(iPath){
   var workingPath = iPath.replace('/', '\\');
   var tabPath = workingPath.split('\\');
@@ -69,6 +120,8 @@ function magicGetModelsFromPath(iPath){
   var fwInterfacesIndex = -1;
   var functionTestsIndex = -1;
 
+  var models = [];
+
   //try path with module
   modIndex = findModuleIndex(tabPath);
   if(modIndex != -1){
@@ -76,7 +129,7 @@ function magicGetModelsFromPath(iPath){
     fwIndex = modIndex - 1;
   }else{
     //try path with a (public,private,protected)Interfaces
-    fwInterfacesIndex = findFWInterfaces(tabPath);
+    fwInterfacesIndex = findFWInterfacesIndex(tabPath);
     if(fwInterfacesIndex != -1){
       wsIndex = fwInterfacesIndex - 2;
       fwIndex = fwInterfacesIndex -1;
@@ -97,24 +150,58 @@ function magicGetModelsFromPath(iPath){
     }
   }
 
+  var newModel;
   if(fwIndex != -1){
     var fwPathTab = tabPath.slice(fwIndex, fwIndex+1);
+    newModel = vt.createModel({
+      type:vt.Types.FW.name,
+      path:Path.normalize(fwPathTab.join('\\')),
+      name:fwPathTab[0]
+    });
+    if(newModel){
+      models.push(newModel);
+    }
     if(modIndex != -1){
       var modPathTab = tabPath.slice(fwIndex, modIndex+1);
+      newModel = vt.createModel({
+        type:vt.Types.MOD.name,
+        path:Path.normalize(modPathTab.join('\\')),
+        name:tabPath[modIndex]
+      });
+      if(newModel){
+        models.push(newModel);
+      }
     }
     var filePathTab = tabPath.slice(fwIndex);
+    var path = Path.normalize(filePathTab.join('\\'));
+    newModel = vt.createModel({
+      type:vt.Types.FILE.name,
+      path:path,
+      name:Path.basename(path)
+    });
+    if(newModel){
+      models.push(newModel);
+    }    
   }else{
-    //fullpath
+    var path = Path.normalize(filePathTab.join('\\'));
+    newModel = vt.createModel({
+      type:vt.Types.FullPath.name,
+      path:path,
+      name:Path.basename(path)
+    });
+    if(newModel){
+      models.push(newModel);
+    }
   }
   
-
+  return models;
 }
 
 
 function bindGui(){
   $('#wizz_get_models').button().click(function(){
     var total_input = $('#wizz_input').val();
-    var models = magicGetModelsFromText(total_input);
+    var models = magicGetModelsFromText2(total_input);
     makeModelsList(models);
   });
   
