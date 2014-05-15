8ee7c8a82c471149627e465368ae303dcb1b6321
shycherry <vinpowful@free.fr>
2014-05-15 21:16:36 +0200

    wip

--- install_links.bat
+++ install_links.bat
@@ -1,3 +1,4 @@
 mklink /D .\bower_components\shy-elt-colorpicker ..\..\shy-elt-colorpicker
 mklink /D .\bower_components\vt-shelltask ..\..\vt-shelltask
 mklink /D .\bower_components\vt-base-element ..\..\vt-base-element
+mklink /D .\bower_components\vt-factory ..\..\vt-factory
