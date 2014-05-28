db093dd98583542322b1aa372eab127241fdf32e
shycherry <vinpowful@free.fr>
2014-05-28 17:30:21 +0200

    add shy-elt-freewall dependency

--- install_links.bat
+++ install_links.bat
@@ -1,2 +1,3 @@
 mklink /D .\bower_components\shy-elt-colorpicker ..\..\shy-elt-colorpicker
 mklink /D .\bower_components\vt-core-elements ..\..\vt-core-elements
+mklink /D .\bower_components\shy-elt-freewall ..\..\shy-elt-freewall
