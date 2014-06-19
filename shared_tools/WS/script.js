36feb5a69a0bee16d21286c841d9b615f0d6d092
shycherry <vinpowful@free.fr>
2014-06-19 23:15:49 +0200

    wip on tools pathes

---
+++ shared_tools/WS/script.js
@@ -0,0 +1,57 @@
+var _dropZone;
+
+function makeWSList(){
+  if(_dropZone){
+    _dropZone.removeAllModels();
+    var models = vt.findModels(function(iModel){
+      return (iModel instanceof vt.Types.WS);
+    });
+    if(models){
+      _dropZone.addModels(models);  
+    }  
+  }  
+}
+
+
+function bindGui(){
+  $('#ws-btn-retrieve-from-name').button().click(function(){
+    var wsName = $('#ws-input-name').val();
+    if(wsName.length != 0){
+      var wsPath = vt.getWSPath(wsName, function(err, data){
+        if(!err){
+          var amorceModel = 
+          {
+            type: vt.Types.WS.name,
+            name : wsName,
+            path : data
+          }
+          var model = vt.createModel(amorceModel);
+          if(model && _dropZone){
+            _dropZone.addModel(model);
+          }
+        }
+      });
+    }
+  });
+
+  $('#ws-btn-create-dummy-with-name').button().click(function(){
+    var wsName = $('#ws-input-name').val();
+    if(wsName.length != 0){
+      var model = vt.createModel({type:vt.Types.WS.name ,name:wsName});
+      if(model && _dropZone){
+        _dropZone.addModel(model);
+      }
+    }
+  });
+
+   _dropZone = makeMultiDroppableZone($('#ws-list'));
+}
+
+exports.load = function(){
+  bindGui();
+  exports.reload();
+}
+
+exports.reload = function(){
+  makeWSList();
+}
