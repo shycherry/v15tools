b802f8c4093272fba4468375eac6141732e3041f
shycherry <vinpowful@free.fr>
2014-11-20 18:38:48 +0100

    wip

--- shared_tools/WS/script.js
+++ shared_tools/WS/script.js
@@ -1,26 +1,35 @@
 var _wsList;
 var _filesToPromoteList;
+var _promoteShell;
+var _currentWS;
 
 function updateFilesToPromoteList(){
-
-  if(_filesToPromoteList){
+  if(_filesToPromoteList)
     _filesToPromoteList.removeAllModels();
-  }  
-  var promoteShell = $('#ws-promote-simul-shell')[0];
-  if (promoteShell)
-  {
-    promoteShell.start();
-    console.log('wesh')
+
+  if (_promoteShell){
+    if(_currentWS){
+      _promoteShell.params = {"test" : _currentWS.name};  
+    }
+    _promoteShell.start();
   }
 }
 
 
 function bindGui(){
-  $('#ws-files-to-promote-btn').click(function(){
-    updateFilesToPromoteList();
-  });
   _wsList = makeMultiDroppableZone($('#ws-list'), vt.Types.WS, 1);
   _filesToPromoteList = makeMultiDroppableZone($('#ws-files-to-promote-list'), vt.Types.FILE);
+  _promoteShell = $('#ws-promote-simul-shell')[0];
+  
+  _wsList.on("models_changed", function(){
+    _currentWS = _wsList.getModels()[0];
+    updateFilesToPromoteList();
+  });
+
+  $('#ws-files-to-promote-btn').click(updateFilesToPromoteList);
+  $("#ws-promote-shelltask")[0].completeCallback = function(err, data){
+    console.log("completeCallback: ",data);
+  }
 }
 
 exports.load = function(){
