ae47238f887fe3eef890b340ed93c03bded81937
shycherry <vinpowful@free.fr>
2014-11-20 17:13:40 +0100

    wip

--- shared_tools/WS/script.js
+++ shared_tools/WS/script.js
@@ -1,47 +1,31 @@
-var _dropZone;
+var _wsList;
+var _filesToPromoteList;
 
-function makeWSList(){
-  if(_dropZone){
-    _dropZone.removeAllModels();
-    var models = vt.findModels(function(iModel){
-      return (iModel instanceof vt.Types.WS);
-    });
-    if(models){
-      _dropZone.addModels(models);  
-    }  
+function updateFilesToPromoteList(){
+
+  if(_filesToPromoteList){
+    _filesToPromoteList.removeAllModels();
   }  
+  var promoteShell = $('#ws-promote-simul-shell')[0];
+  if (promoteShell)
+  {
+    promoteShell.start();
+    console.log('wesh')
+  }
 }
 
 
 function bindGui(){
-  $('#ws-btn-retrieve-from-name').click(function(){
-    var wsName = $('#ws-input-name').val();
-    if(wsName.length != 0){
-      var wsPath = vt.getWSPath(wsName, function(err, data){
-        if(!err){
-          var amorceModel = 
-          {
-            type: vt.Types.WS.name,
-            name : wsName,
-            path : data
-          }
-          var model = vt.createModel(amorceModel);
-          if(model && _dropZone){
-            _dropZone.addModel(model);
-          }
-        }
-      });
-    }
+  $('#ws-files-to-promote-btn').click(function(){
+    updateFilesToPromoteList();
   });
-
-   _dropZone = makeMultiDroppableZone($('#ws-list'));
+  _wsList = makeMultiDroppableZone($('#ws-list'), vt.Types.WS, 1);
+  _filesToPromoteList = makeMultiDroppableZone($('#ws-files-to-promote-list'), vt.Types.FILE);
 }
 
 exports.load = function(){
   bindGui();
-  exports.reload();
 }
 
 exports.reload = function(){
-  makeWSList();
 }
