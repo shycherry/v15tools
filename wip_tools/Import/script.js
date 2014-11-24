4baa539987df97c57627de7d8ef53658d8f16ec6
shycherry <vinpowful@free.fr>
2014-11-24 18:25:15 +0100

    wip

--- wip_tools/Import/script.js
+++ wip_tools/Import/script.js
@@ -1,11 +1,39 @@
 var _fromWSDropZone;
 var _toWSDropZone;
 var _filesDropZone;
+var _importShell;
+var _importFilesBtn;
+var _currentFromWS;
+var _currentToWS;
+
+function updateShellParams(){
+  _importShell.params = {
+    "ws_from" : _currentFromWS ? _currentFromWS.name : '',
+    "ws_to" : _currentToWS ? _currentToWS.name : ''
+  };
+}
 
 function bindGui(){
   _fromWSDropZone = makeMultiDroppableZone($('#import-from-ws-list'), vt.Types.WS, 1); 
   _toWSDropZone = makeMultiDroppableZone($('#import-to-ws-list'), vt.Types.WS, 1);
   _filesDropZone = makeMultiDroppableZone($('#import-files-list'), vt.Types.FILE);
+  _importShell = $('#import-shell')[0];
+  _importFilesBtn = $('#import-btn-go')[0];
+
+  _fromWSDropZone.on("models_changed", function(){
+    _currentFromWS = _fromWSDropZone.getModels()[0];
+    updateShellParams();
+  });
+
+  _toWSDropZone.on("models_changed", function(){
+    _currentToWS = _toWSDropZone.getModels()[0];
+    updateShellParams();
+  });
+
+  _importFilesBtn.onclick = function(){
+    _importShell.start();
+  }
+
 }
 
 exports.load = function(){
