2e76952cf46cfb4c302e53d263e5a34cbd152a1c
shycherry <vinpowful@free.fr>
2014-12-02 18:29:15 +0100

    wip

--- shared_tools/WS/script.js
+++ shared_tools/WS/script.js
@@ -2,6 +2,8 @@ var _wsList;
 var _filesToPromoteList;
 var _filesToPromoteBtn;
 var _promoteButtonBatch;
+var _initShellTask;
+var _chWsShellTask;
 var _promoteShellTask;
 var _currentWS;
 
@@ -15,19 +17,30 @@ function bindGui(){
   _filesToPromoteList = makeMultiDroppableZone($('#ws-files-to-promote-list'), vt.Types.FILE);
   _promoteButtonBatch = $('#ws-promote-simul-buttonbatch')[0];
   _promoteShellTask = $("#ws-promote-shelltask")[0];
+  _initShellTask = $("#ws-init-shelltask")[0];
+  _chWsShellTask = $("#ws-ch-ws-shelltask")[0];
 
   _wsList.on("models_changed", function(){
     _currentWS = _wsList.getModels()[0];
     if(_currentWS){
-      _promoteButtonBatch.params = {"ws" : _currentWS.name};  
+      _promoteButtonBatch.params = {"ws" : _currentWS.name};
+      _chWsShellTask.activated = true;
     }
-    _filesToPromoteList.removeAllModels();
   });
 
   _promoteButtonBatch.addEventListener('task_finished', function(ev){
-    if(ev.detail.src && ev.detail.src.id == 'ws-promote-shelltask'){
+    if(!ev.detail.src && ev.detail.src.id)
+      return;
+    
+    var sourceId = ev.detail.src.id;
+
+    if(sourceId == 'ws-promote-shelltask'){
       var models = vt. magicGetModelsFromText(ev.detail.data);
       updateFilesToPromoteList(models);
+    }else if(sourceId == 'ws-init-shelltask'){
+      _initShellTask.activated = false;
+    }else if(sourceId == 'ws-ch-ws-shelltask'){
+      _chWsShellTask.activated = false;
     }
   });
 }
