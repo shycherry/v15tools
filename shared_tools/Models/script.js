ddd5757f35f5af29c9ef38a07f7faa02b6e086d7
shycherry <vinpowful@free.fr>
2014-12-08 17:15:09 +0100

    wip models

--- shared_tools/Models/script.js
+++ shared_tools/Models/script.js
@@ -1,4 +1,7 @@
 var _dropZone;
+var _retrieveWSFromNameButtonBatch;
+var _modelsInitShellTask;
+var _modelsDsWSShellTask;
 
 function makeWSList(){
   if(_dropZone){
@@ -7,34 +10,84 @@ function makeWSList(){
       return (iModel instanceof vt.Types.WS);
     });
     if(models){
-      _dropZone.addModels(models);  
-    }  
-  }  
+      _dropZone.addModels(models);
+    }
+  }
 }
 
 
 function bindGui(){
-  $('#models-ws-btn-retrieve-from-name').click(function(){
-    var wsName = $('#models-ws-input-name').val();
-    if(wsName.length != 0){
-      var wsPath = vt.getWSPath(wsName, function(err, data){
-        if(!err){
-          var amorceModel = 
-          {
-            type: vt.Types.WS.name,
-            name : wsName,
-            path : data
-          }
-          var model = vt.createModel(amorceModel);
-          if(model && _dropZone){
-            _dropZone.addModel(model);
-          }
-        }
-      });
+  _dropZone = makeMultiDroppableZone($('#models-ws-list'));
+  _retrieveWSFromNameButtonBatch = document.querySelector('#models-ws-btn-retrieve-from-name');
+  _modelsInitShellTask = document.querySelector('#models-init-shelltask');
+  _modelsDsWSShellTask = document.querySelector('#models-ds-ws-shelltask');
+
+  _retrieveWSFromNameButtonBatch.addEventListener('task_finished', function(ev){
+    if(!ev.detail.src || !ev.detail.src.id)
+      return;
+
+    var source = ev.detail.src;
+    var sourceId = ev.detail.src.id;
+
+    if(sourceId == _modelsInitShellTask.id){
+      _modelsInitShellTask.activated = false;
+    }
+
+    if(sourceId == _modelsDsWSShellTask.id){
+      var winImagePathMatch = /WINDOWS.+(\\\\.+)/i.exec(ev.detail.data);
+      if(!winImagePathMatch){
+        //error
+        return;
+      }
+      winImagePathMatch = winImagePathMatch[1];
+      var amorceModel = {
+        type: vt.Types.WS.name,
+        name : source.params['ws_name'],
+        path : winImagePathMatch
+      }
+      var model = vt.createModel(amorceModel);
+      if(model && _dropZone){
+        _dropZone.addModel(model);
+      }
+    }
+
+  });
+
+  _retrieveWSFromNameButtonBatch.addEventListener('click', function(ev){
+    if(!ev.detail.src || !ev.detail.src.id)
+      return;
+    var sourceId = ev.detail.src.id;
+    console.log('wesh')
+    if(sourceId == _retrieveWSFromNameButtonBatch.id){
+      console.log('yo')
+      var wsName = $('#models-ws-input-name').val();
+      console.log('pwet ', wsName)
+      _retrieveWSFromNameButtonBatch.params = {'ws_name' : wsName};
     }
   });
 
-  _dropZone = makeMultiDroppableZone($('#models-ws-list'));
+
+  // $('#models-ws-btn-retrieve-from-name').click(function(){
+  //   var wsName = $('#models-ws-input-name').val();
+  //   if(wsName.length != 0){
+  //     var wsPath = vt.getWSPath(wsName, function(err, data){
+  //       if(!err){
+  //         var amorceModel =
+  //         {
+  //           type: vt.Types.WS.name,
+  //           name : wsName,
+  //           path : data
+  //         }
+  //         var model = vt.createModel(amorceModel);
+  //         if(model && _dropZone){
+  //           _dropZone.addModel(model);
+  //         }
+  //       }
+  //     });
+  //   }
+  // });
+
+
 }
 
 exports.load = function(){
