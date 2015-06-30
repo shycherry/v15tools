d1a412f7c98387a3a711579b7bcbf2ee0cf58d4e
shycherry <vinpowful@free.fr>
2015-06-30 14:39:33 +0200

    wip WS

--- shared_tools/WS/script.js
+++ shared_tools/WS/script.js
@@ -3,34 +3,41 @@ var _wsList;
 var _filesToPromoteList;
 var _fileToForgetList;
 var _filesToPromoteBtn;
-var _promoteButtonBatch;
+var _promoteSimulButtonBatch;
 var _forgetChgButtonBatch;
 var _currentWS;
 
 function bindGui(){
   _wsList = makeMultiDroppableZone($('#ws-list'), vt.Types.WS, 1);
-  _fileToForgetList = makeMultiDroppableZone($('#ws-file-list'), vt.Types.FILE);
-
+  _fileToForgetList = makeMultiDroppableZone($('#ws-file-list'), vt.Types.FILE, 1);
   _filesToPromoteList = makeMultiDroppableZone($('#ws-files-to-promote-list'), vt.Types.FILE);
   _filesToPromoteList[0].hidden = true;
+
   _chWsButtonBatch = $('#ws-ch-ws-buttonbatch')[0];
-  _promoteButtonBatch = $('#ws-promote-simul-buttonbatch')[0];
+  _promoteSimulButtonBatch = $('#ws-promote-simul-buttonbatch')[0];
   _forgetChgButtonBatch = $('#ws-forget-changes-buttonbatch')[0];
+  _publishButtonBatch = $('#ws-publish-buttonbatch')[0];
+  _syncButtonBatch = $('#ws-sync-buttonbatch')[0];
+  _solveMergeButtonBatch = $('#ws-solve-merge-buttonbatch')[0];
 
-  _promoteButtonBatch.setWorker(_chWsButtonBatch.getWorker());
+  _promoteSimulButtonBatch.setWorker(_chWsButtonBatch.getWorker());
   _forgetChgButtonBatch.setWorker(_chWsButtonBatch.getWorker());
+  _publishButtonBatch.setWorker(_chWsButtonBatch.getWorker());
+  _syncButtonBatch.setWorker(_chWsButtonBatch.getWorker());
+  _solveMergeButtonBatch.setWorker(_chWsButtonBatch.getWorker());
 
-  reloadWS();
-
-  function reloadWS(){
-    _currentWS = _wsList.getModels()[0];
-    _chWsButtonBatch.params  = _.extend({}, _.extend(_chWsButtonBatch.params, {"ws" : _currentWS? _currentWS.name : null}));
-    _promoteButtonBatch.params = _.extend({}, _.extend(_promoteButtonBatch.params, {"ws" : _currentWS? _currentWS.name : null}));
-    _forgetChgButtonBatch.params = _.extend({}, _.extend(_forgetChgButtonBatch.params, {"ws" : _currentWS? _currentWS.name : null}));
+  function dispatchWS(iWS){
+    _promoteSimulButtonBatch.params = _.extend({}, _.extend(_promoteSimulButtonBatch.params, {"ws" : iWS? iWS.name : null}));
+    _forgetChgButtonBatch.params = _.extend({}, _.extend(_forgetChgButtonBatch.params, {"ws" : iWS? iWS.name : null}));
+    _publishButtonBatch.params = _.extend({}, _.extend(_publishButtonBatch.params, {"ws" : iWS? iWS.name : null}));
+    _syncButtonBatch.params = _.extend({}, _.extend(_syncButtonBatch.params, {"ws" : iWS? iWS.name : null}));
+    _solveMergeButtonBatch.params = _.extend({}, _.extend(_solveMergeButtonBatch.params, {"ws" : iWS? iWS.name : null}));
   }
 
   _wsList.on("models_changed", function(){
-    reloadWS();
+    _currentWS = _wsList.getModels()[0];
+    _chWsButtonBatch.params  = _.extend({}, _.extend(_chWsButtonBatch.params, {"ws" : _currentWS? _currentWS.name : null}));
+    dispatchWS(_currentWS);
   });
 
   _fileToForgetList.on("models_changed", function(){
@@ -47,22 +54,22 @@ function bindGui(){
     }
   });
 
-  _promoteButtonBatch.addEventListener('batch_started', function(ev){
+  _promoteSimulButtonBatch.addEventListener('batch_started', function(ev){
     _filesToPromoteList.removeAllModels();
   });
 
-  _promoteButtonBatch.setTaskFinishedCallback('ws-promote-simul-shelltask', function(ev){
+  _promoteSimulButtonBatch.setTaskFinishedCallback('ws-promote-simul-shelltask', function(ev){
     var models = vt. magicGetModelsFromText(ev.detail.data);
     _filesToPromoteList.addModels(models);
   });
 
   _chWsButtonBatch.addEventListener('batch_started', function(ev){
-    _promoteButtonBatch.params = {};
+    dispatchWS(null);
   });
 
   _chWsButtonBatch.setTaskFinishedCallback("ws-ch-ws-shelltask", function(ev){
     _chWsButtonBatch.disable();
-    _promoteButtonBatch.params = _currentWS ? {"ws" : _currentWS.name} : {};
+    dispatchWS(_currentWS);
   });
 
 }
